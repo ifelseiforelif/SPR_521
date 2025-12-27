8b69a15ff543dacb407c50015f011766fa6b898c
Natalya Babenko <ifelseiforelif@gmail.com>
2025-12-27 10:35:09 +0200

    added methods for string

--- Less14_string/Source.cpp
+++ Less14_string/Source.cpp
@@ -6,8 +6,59 @@ using namespace std;
 
 int main()
 {
-	Document doc1{"main", 340, "docx"};
-	doc1.show();
+	try
+	{
+		string test = "Hello";
+		int index;
+		cin >> index;
+		if (index >= 0 && index < test.size())
+		{
+			cout << test[index];
+		}
+		cout << test.at(10)<< endl;
+		test.insert(0, "aa");
+		test.insert(test.size(), "bb");
+		cout << test;
+	}
+	catch (exception& ex)
+	{
+		cout << ex.what();
+	}
+	
+
+	/*test.erase(0,2);*/
+
+	return 0;
+	string name="";
+	cout << "Enter name: ";
+	getline(cin, name);
+	if (name.empty())
+	{
+		cout << "string is empty\n";
+	}
+	else
+	{
+		cout << "Welcome " << name << endl;
+	}
+	string s1 = "Hello";
+	string s2 = s1; //тут глибока копія
+	s1[0] = 'B';
+	cout << s2 << endl;
+	string s3 = move(s2); //s2 буде empty
+	cout << s3 << endl;
+	cout << s2.length() << endl; //також розмір рядка
+	s3.clear(); //очистка рядка
+	cout << s3.length() << endl;
+	s1.erase(3); //видалення 
+	cout << s1 << endl;
+	s1.insert(0, "a"); //вставка рядка
+	cout << s1 << endl;
+	/*string::iterator it = s1.begin();
+	cout << *it;*/
+
+	
+	//Document doc1{"main", 340, "docx"};
+	//doc1.show();
 	//basic_string<char> mes2;
 	//string mes = "Hello"; //heap
 	//cout << mes.size()<< endl; //розмір
@@ -17,6 +68,7 @@ int main()
 	//mes += "!";
 	//cout << mes.capacity() << endl;
 	//+ конкатенація, +=
+
 
 	return 0;
 }
