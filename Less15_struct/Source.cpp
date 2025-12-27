49db0020b38e407249d791365556b6ab051a1f59
Natalya Babenko <ifelseiforelif@gmail.com>
2025-12-27 13:09:24 +0200

    added agregation and composition

---
+++ Less15_struct/Source.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <stack>
+#include <queue>
+
+using namespace std;
+struct Point
+{
+	int x;
+	int y;
+	void show()
+	{
+		cout << x << " " << y << endl;
+	}
+};
+
+int main()
+{
+	stack<Point> my_point_stack;
+
+	my_point_stack.push({5,6});
+	my_point_stack.push({1,-3});
+	my_point_stack.push({0,1});
+	while (!my_point_stack.empty())
+	{
+		my_point_stack.top().show();
+		my_point_stack.pop();
+	}
+	stack<int> my_stack;
+	my_stack.push(12);
+	my_stack.push(1);
+	my_stack.push(-1);
+	while (!my_stack.empty())
+	{
+		cout << my_stack.top() << ' ';
+		my_stack.pop();
+	}
+	cout << "Size: " << my_stack.size() << endl;
+	return 0;
+}
---
+++ Less16_agregation_composition/Source.cpp
@@ -0,0 +1,83 @@
+﻿#include <iostream>
+using namespace std;
+//Агрегація та композиція
+
+
+//Людина-brain(композиція)  шапка
+//Смартфон-процессор(композиція) стилус
+//Квартира-стіни(композиція) меблі
+class Brain
+{
+private:
+	float weight;
+public:
+	Brain() :weight{10}
+	{
+
+	}
+	Brain(float weight) :weight{ weight } {}
+	void setWeight(float weight)
+	{
+		this->weight = weight;
+	}
+	float getWeight()
+	{
+		return weight;
+	}
+};
+class Hat
+{
+private:
+	string color;
+public:
+	Hat() :color{ "white"}
+	{
+
+	}
+	Hat(string color) :color{color } {}
+	string getColor()
+	{
+		return color;
+	}
+
+};
+
+class Person
+{
+private:
+	string name;
+	unsigned short age;
+	Hat* hat;
+	Brain brain;
+public:
+	Person(string name,unsigned short age, Hat* hat, float weight) 
+	{
+		this->name = name;
+		this->age = age;
+		this->hat = hat;
+		this->brain.setWeight(weight);
+	}
+	void print()
+	{
+		cout << "Name: " << name << " Age: " << age << " Hat: " << hat->getColor() << " Brain: " << brain.getWeight()  << endl;
+	}
+};
+int main()
+{
+	//Агрегація
+	Hat hat("red");
+
+	{
+		Person p1("Alex", 20, &hat, 20);
+		p1.print();
+	}
+	hat.getColor();
+	/*Person* p1 = new Person("Alex", 20, &hat, 20);
+	p1->print();
+	delete p1;
+	p1 = new Person("Bob", 23, &hat, 30);
+	p1->print();
+	delete p1;*/
+
+	return 0;
+}
